e761987291c11df41907a402de6704e6eb2129c8
Akila <akilarajesh12@g.ucla.edu>
2025-08-16 14:05:23 -0700

    ^

--- client/src/context/PiecesContext.jsx
+++ client/src/context/PiecesContext.jsx
@@ -1,5 +1,6 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
 import axios from "axios";
+const API_URL = import.meta.env.VITE_API_URL;
 
 // Create context
 const PiecesContext = createContext([]);
